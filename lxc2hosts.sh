71707ba0802b53f0d65605d2f783aeb8c6c92a01
René Thiemann <rene.thiemann@immoware24.de>
2022-03-10 10:58:25 +0100

    added lxc2hosts script

---
+++ lxc2hosts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function checkRoot() {
+  ME=$(whoami)
+  if [ ! "root" == $ME ]; then
+    echo "you must be root!"
+    echo "you are $ME. you rock, sure, but root rocks more ;)"
+    echo ""
+    exit 1
+  fi
+}
+
+checkRoot
+
+if ! command -v jq &>/dev/null; then
+  echo "jq could not be found"
+  echo "run"
+  echo "sudo apt install jq"
+  echo "to install"
+  exit 1
+fi
+
+echo "" >>/etc/hosts
+echo "#######" >>/etc/hosts
+echo "" >>/etc/hosts
+lxc list --format=json | jq -r '.[] | .state.network.eth0.addresses[0].address + " " +  .name' >>/etc/hosts
+
+exit 0
