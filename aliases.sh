8a7790952ae914922157b6b83c28c6f3ddda3359
wolxXx <devops@wolxXx.de>
2024-09-19 18:16:54 +0200

    add dockerinfo bash alias

---
+++ aliases.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+alias dockerinfo="docker ps -qa | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }}' | sed 's/ \// /' | sort && docker ps -a"
