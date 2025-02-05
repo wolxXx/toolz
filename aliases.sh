596d972a35eddcbfaadd77cff4c5e93ee773b0bc
wolxXx <devops@wolxXx.de>
2025-02-05 10:50:47 +0100

    extend git status shortcut i

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 alias dockerinfo="docker ps -qa | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }}' | sed 's/ \// /' | sort && docker ps -a"
-alias i="git status && git branch"
+alias i="git config user.email && git config user.name && git branch && git status"
