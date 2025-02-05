ba7100d4e50e79fdf8f4de09d487844f93cf142f
wolxXx <devops@wolxXx.de>
2025-02-05 07:02:45 +0100

    Add a git status and branch alias for convenience
    This introduces the 'i' alias to quickly check git status and branch. It enhances developer productivity by reducing repetitive commands.

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 alias dockerinfo="docker ps -qa | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }}' | sed 's/ \// /' | sort && docker ps -a"
+alias i="git status && git branch"
