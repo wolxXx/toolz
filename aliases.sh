98a1784abb6ac04cbcba87d708531b212cadb837
wolxXx <devops@wolxXx.de>
2025-09-08 22:30:07 +0200

    Refactor `dockerinfo` alias into a function for enhanced readability and flexibility
    Replaced the `dockerinfo` alias with the `fun_dockerinfo` function to improve maintainability and allow parameterized filtering of Docker container details (e.g., IP, name, status). Maintained backward compatibility with the original alias.

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-alias dockerinfo="docker ps -qa | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }}' | sed 's/ \// /' | sort && docker ps -a"
+fun_dockerinfo() {
+  k=1
+  
+  if [ "$1" = "--ip" ]; then
+    k=4
+  fi
+  if [ "$1" = "--name" ]; then
+    k=1
+  fi
+  if [ "$1" = "--status" ]; then
+    k=2
+  fi
+  if [ "$1" = "--image" ]; then
+    k=3
+  fi
+  if [ "$1" = "--network" ]; then
+    k=5
+  fi
+  
+   for cid in $(docker ps -aq); do
+      docker inspect --format '{{.Name}} {{.State.Status}} {{.Config.Image}} {{range $k,$v := .NetworkSettings.Networks}}{{$v.IPAddress}} ({{$k}}){{end}}' "$cid"  | sed 's#^/##'
+    done | awk '{ if ($3=="") $3="N/A"; print }' | column -t | sort -k$k
+}
+
+alias dockerinfo="fun_dockerinfo"
 alias i="git remote -v show && git config user.email && git config user.name && git remote update && git branch && git status"
